f4689cfbac75d59cd56a5ead715e76470e163b18
Iddan <Iddan@IDDAN-HOME-PC>
2021-08-29 17:51:44 +0300

    Add project files.

---
+++ pet-store/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pet_store.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
