5f760138a9ef3c49c90e8042a36041b3dd14b8fe
Itai Levy <sasonitai@gmail.com>
2021-09-04 17:29:45 +0300

    base models

---
+++ pet-store/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pet_store.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public Category Parent { get; set; }
+
+        public string Name { get; set; }
+    }
+}
