4b7eca2df5aafbe588f9fdd0be9dc719fd41da85
Amit Sela <lgg2amit95@cs.colman.ac.il>
2021-09-20 18:36:26 +0300

    Canvas at product view

---
+++ pet-store/wwwroot/js/ProductCanvas.js
@@ -0,0 +1,9 @@
+﻿var canvas = document.getElementById("myCanvas");
+
+var ctx = canvas.getContext("2d");
+
+    ctx.font = "30px Comic Sans MS";
+    ctx.fillStyle = "#128ced";
+    ctx.textAlign = "center";
+    ctx.fillText("Try Out Our Products!", canvas.width / 2, canvas.height / 2);
+
