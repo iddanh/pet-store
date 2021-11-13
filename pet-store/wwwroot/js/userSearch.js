64979cf9f7c3b04cfbee02f786a3a76da3204daa
Amit Zilber <sk8amit@gmail.com>
2021-11-13 13:59:08 +0200

    user search update

---
+++ pet-store/wwwroot/js/userSearch.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    $('.btnSearch').click(function () {
+        var name = $('input[name="name"]');
+        var email = $('input[name="email"]');
+        var type = $('select[name="type"]');
+        var company = $('select[name="company"]');
+        var token = $('input[name="__RequestVerificationToken"]').val();
+
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "/Users/UserSearch",
+            data: {
+                name: name.val(),
+                email: email.val(),
+                company: company.val(),
+                type: type.val(),
+                __RequestVerificationToken: token
+            }
+        }).done(function (result) {
+            var tbody = $('tbody');
+            var template = $('#template').html();
+            tbody.html('');
+            $.each(result, function (key, value) {
+                var temp = template;
+                $.each(value, function (key, value) {
+                    temp = temp.replaceAll('${' + key + '}', value);
+                });
+                tbody.append(temp);
+            });
+        });
+    });
+});
